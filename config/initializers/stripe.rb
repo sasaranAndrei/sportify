a7c0ec027ba4c949645c42213c970c86057e76cc
Andrei Sasaran <sasaranandreipaul@gmail.com>
2022-03-13 16:46:22 +0200

    Update Stripe TEST MODE credentials for Production

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
 
-environment = :development || Rails.env.to_sym # ultima parte daca merge si in productie
-
+environment = Rails.env.to_sym
 Stripe.api_key = Rails.application.credentials.dig(environment, :stripe, :private_key)
