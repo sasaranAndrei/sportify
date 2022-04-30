e4c3c45e36cf7d4b33659a6fb7de5b22d4b2860e
Andrei Sasaran <sasaranandreipaul@gmail.com>
2022-04-30 14:54:15 +0300

    Add GMAPS basic functionality

--- app/javascript/arenas.js
+++ app/javascript/arenas.js
@@ -1,3 +1,7 @@
+const MAP_CENTER_LATITUDE = 46.75550;
+const MAP_CENTER_LONGITUDE = 23.59950;
+var selectedArenaTitle = -1; // no arena selected yet
+
 window.onload = function() {
   url = window.location.href;
   console.log(url);
@@ -7,31 +11,34 @@ window.onload = function() {
   }
 }
 
-function getArenasLocations() {
-  arenas = $.ajax({
+function initGoogleMaps() {
+  $.ajax({
     url: '/arenas',
     type: 'get',
     data: '',
-    success: function() { alert('Success GET Arenas'); },
-    error: function() { alert('Error GET Arenas'); } 
+    dataType: 'json',
+    success: function(data) { drawMap(data) },
+    error: function() { console.log('Error GET Arenas'); },
   });
 }
 
-function initGoogleMaps() {
-  var locations = getArenasLocations();
-  
+function extractLocationsFromArenas(arenas) {
+  locations = [];
 
-  var locations = [
-    ['Bondi Beach', -33.890542, 151.274856, 4],
-    ['Coogee Beach', -33.923036, 151.259052, 5],
-    ['Cronulla Beach', -34.028249, 151.157507, 3],
-    ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
-    ['Maroubra Beach', -33.950198, 151.259302, 1]
-  ];
+  for (arena of arenas) {
+    arenaData = [arena.title, arena.latitude, arena.longitude, arena.id];
+    locations.push(arenaData);
+  }
+
+  return locations;
+}
+
+function drawMap(arenas) {
+  var locations = extractLocationsFromArenas(arenas);
   
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 10,
-    center: new google.maps.LatLng(-33.92, 151.25),
+    zoom: 13,
+    center: new google.maps.LatLng(MAP_CENTER_LATITUDE, MAP_CENTER_LONGITUDE),
     mapTypeId: google.maps.MapTypeId.ROADMAP
   });
   
@@ -49,6 +56,8 @@ function initGoogleMaps() {
       return function() {
         infowindow.setContent(locations[i][0]);
         infowindow.open(map, marker);
+        selectedArenaTitle = locations[i][3];
+        console.log('Select : ' + selectedArenaTitle);
       }
     })(marker, i));
   }  
