d8c3595f1eba1732c97c7322644ddd9133d6f582
Andrei Sasaran <sasaranandreipaul@gmail.com>
2022-05-03 22:09:41 +0300

    Add validation for reservaiton owner

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,7 +3,7 @@
 class Player < ApplicationRecord
   belongs_to :user, dependent: :destroy
 
-  has_many :own_reservations, class_name: 'Reservation'
+  has_many :own_reservations, class_name: 'Reservation', foreign_key: 'owner_player_id'
   has_many :reservation_players
   has_many :guest_reservations, class_name: 'Reservation', through: :reservation_players, source: :reservation
 end
--- app/models/reservation_player.rb
+++ app/models/reservation_player.rb
@@ -1,4 +1,14 @@
 class ReservationPlayer < ApplicationRecord
   belongs_to :reservation
   belongs_to :player
+
+  validate :owner_absence_from_guests_list
+
+  private
+
+    def owner_absence_from_guests_list
+      if player == reservation.owner_player
+        errors.add(:reservation_owner, "must join. He can't be present in the Guests list too") 
+      end
+    end
 end
