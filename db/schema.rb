3369c062ab61a4b2f590a529b573145898d20a8c
sasaranAndrei <56340401+sasaranAndrei@users.noreply.github.com>
2022-05-14 13:49:13 +0300

    Feature/14/accept invitation (#18)
    * Add changes
    
    * Add Accept Invitation
    
    * Add joined_by field
    
    * Add joined_by flow

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_13_164020) do
+ActiveRecord::Schema.define(version: 2022_05_14_102043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -141,6 +141,7 @@ ActiveRecord::Schema.define(version: 2022_05_13_164020) do
     t.bigint "player_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "joined_by"
     t.index ["player_id"], name: "index_reservation_players_on_player_id"
     t.index ["reservation_id"], name: "index_reservation_players_on_reservation_id"
   end
