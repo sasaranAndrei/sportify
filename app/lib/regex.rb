50eb40493352464fcd274074299669e58eaa1d98
Andrei Sasaran <sasaranandreipaul@gmail.com>
2022-05-26 19:40:58 +0300

    Change class to module

--- app/lib/regex.rb
+++ app/lib/regex.rb
@@ -1,5 +1,5 @@
-# TechQuestion? - How to name this class?
+# TechQuestion? - How to name this module / class?
 
-class Regex
+module Regex
   ROMANIAN_PHONE_NUMBER = /\A(\+4|)?(07[0-8]{1}[0-9]{1}|02[0-9]{2}|03[0-9]{2}){1}?(\s|\.|\-)?([0-9]{3}(\s|\.|\-|)){2}\z/
 end
