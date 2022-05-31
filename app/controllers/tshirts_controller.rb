5476fd18fdf05a83a0d897ebfdeb85938000f130
Andrei Sasaran <sasaranandreipaul@gmail.com>
2022-05-31 14:28:43 +0300

    Add logic for Tshirt too

--- app/controllers/tshirts_controller.rb
+++ app/controllers/tshirts_controller.rb
@@ -1,5 +1,26 @@
 class TshirtsController < ApplicationController
   def new
-    
+    @tshirt = Tshirt.new
   end
+
+  def create
+    # TODO: Refactor!
+    @tshirt = Tshirt.new(tshirt_params)
+
+    return redirect_back fallback_location: sportify_coins_shop_path, notice: "You don't have enough SCoins to buy a Tshirt" unless current_user.can_pay?(@tshirt.price)
+
+    if @tshirt.save
+      current_user.remove_tokens!(@tshirt.price)
+
+      redirect_to collection_player_path(current_user.player), notice: 'Tshirt was successfully purchased'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def tshirt_params
+      params.require(:tshirt).permit(:color, :size, :nickname, :number).merge(player_id: current_user.player.id)
+    end
 end
--- app/models/tshirt.rb
+++ app/models/tshirt.rb
@@ -3,12 +3,14 @@ class Tshirt < ApplicationRecord
   extend Sellable
 
   COLORS = %w(white green red blue maroon orange).freeze
+  SIZES = %w(XXS XS S M L XL XXL).freeze
 
   belongs_to :player
   has_one_attached :photo
 
   # TechQuestion? - How can I extract this common behaviour in a ColorValidator
   validates :color, inclusion: { in: COLORS }
+  validates :size, inclusion: { in: SIZES }
 
   def self.sample
     # TechQuestion? - Add @@sample ||= ?
