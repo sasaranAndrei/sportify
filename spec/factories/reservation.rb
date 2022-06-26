8474c940de8069c96de2ebc2022d9da24809732b
Andrei Sasaran <sasaranandreipaul@gmail.com>
2022-06-26 20:01:19 +0300

    Add some tests for reservation

--- spec/factories/reservation.rb
+++ spec/factories/reservation.rb
@@ -1,13 +1,20 @@
 FactoryBot.define do
   factory :reservation do
-    booking_date { Faker::Name.name }
-    booking_hour { Faker::Name.name }
+    booking_date { Date.today }
+    booking_hour { 20 }
 
-    # TODO: cum fac sa mock-uiesc un user
+    association :field
+    association :owner_player, factory: :player
+
+    # TODO: cum fac sa mock-uiesc un player si un field
     # ideea e ca am nevoie de ceva care sa poata raspunda la
     # .avatar, .email, .remove_tokens!, 
     # check cu cartea in paralel
     # si cu proiectele de la lucru in paralel
+
+    trait :passed do
+      booking_date { Date.yesterday }
+    end
 
     trait :with_invitation_token do
       invitation_token { SecureRandom.hex(10) } # TechQuestion? - E ok sa fac asta? Cand se ruleaza testu si se adauga chestii in test_db se ruleaza codu asta? ca trebe sa fie unic ala check app/models/reservation.rb
--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -5,9 +5,39 @@ RSpec.describe Reservation, type: :model do
     expect(build(:reservation)).to be_valid
   end
 
-  describe 'Class methods' do
-    describe '.has_passed?' do
+  describe 'Instance methods' do
+    # TODO: oare pt astea de mai jos trebe teste? daca da, cum? :-)
+    # describe '#all_players' do 
+    # end
+    # describe '#place' do 
+    # end
+    # describe '#participate?' do #?
+    # end
 
+    describe '#days_before_booking_date' do 
+      
+    end
+
+    describe '#free_slots' do
+
+    end
+
+    describe '#has_passed?' do
+      context 'passed reservation' do
+        # poate ar fii mai bine sa l mut in subject si sa fac update in before
+        let(:reservation) { build(:reservation, :passed) }
+        
+        it 'returns true' do
+          # byebug
+          expect(reservation.has_passed?).to be true
+        end
+      end
+
+      context 'upcoming reservation' do
+        it 'return false' do
+          expect(false).to be false
+        end
+      end
     end
   end
 end
