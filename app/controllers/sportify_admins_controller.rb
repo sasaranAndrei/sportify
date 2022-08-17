c57331ecfee49eea958e02f30f54dec4809e0233
Andrei Sasaran <sasaranandreipaul@gmail.com>
2022-08-17 16:20:32 +0300

    Add http basic authentication

--- app/controllers/sportify_admins_controller.rb
+++ app/controllers/sportify_admins_controller.rb
@@ -1,5 +1,6 @@
 class SportifyAdminsController < ApplicationController
   skip_before_action :authenticate_user!
+  http_basic_authenticate_with name: ENV['ADMIN_NAME'], password: ENV['ADMIN_PASSWORD']
 
   def show
   end
